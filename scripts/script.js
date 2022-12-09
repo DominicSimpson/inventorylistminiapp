a1d6c3c15c09ffcac4ba2016ec2f0b33414a3399
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-12-09 22:22:51 +0000

    Functionality development

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,22 @@ let categoriesList = JSON.parse(localStorage.getItem('userTasks')) || []; // Arr
                                                                          // user's inputted categories
 
 // <========== Function that retrieves list of categories from local storage to update DOM  ==========> 
-const getCategoriesFromLocalStorage = (categories) => {
-    // Create a new <li> element and give it a class and unique ID
+const getCategoriesFromLocalStorage = (category) => {
+    // Create a new <li> element and give it both a class property and unique ID
     const categoryList = document.createElement("li");
     categoryList.classList = "listItem";
-    taskItem.dataset.
+    categoryList.dataset.taskNum = category.id;
+
+    // Creates the content inside the <li> element
+    categoryList.innerHTML = `
+    <input aria-label="Mark category as complete" type="checkbox"/>
+    <span>${category.description}</span>
+    <button type="button" class="remove-task-btn"><i class="fa-solid fa-x"></i></button>
+    `;
+
+   // Adds the <li> element from the storage to the DOM
+    list.appendChild(categoryList);
+
+    const checkbox = categoryList.querySelector("input[type='checkbox']");
+
 }
