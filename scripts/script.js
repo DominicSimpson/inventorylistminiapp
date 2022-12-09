23e79913253f904715bc81037234d42729446ff9
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-12-09 22:55:42 +0000

    JS functionality

--- scripts/script.js
+++ scripts/script.js
@@ -10,20 +10,41 @@ let categoriesList = JSON.parse(localStorage.getItem('userTasks')) || []; // Arr
 // <========== Function that retrieves list of categories from local storage to update DOM  ==========> 
 const getCategoriesFromLocalStorage = (category) => {
     // Create a new <li> element and give it both a class property and unique ID
-    const categoryList = document.createElement("li");
-    categoryList.classList = "listItem";
-    categoryList.dataset.taskNum = category.id;
+    const categoryListItem = document.createElement("li");
+    categoryListItem.classList = "listItem";
+    categoryListItem.dataset.taskNum = category.id;
 
     // Creates the content inside the <li> element
-    categoryList.innerHTML = `
+    categoryListItem.innerHTML = `
     <input aria-label="Mark category as complete" type="checkbox"/>
     <span>${category.description}</span>
     <button type="button" class="remove-task-btn"><i class="fa-solid fa-x"></i></button>
     `;
 
    // Adds the <li> element from the storage to the DOM
-    list.appendChild(categoryList);
+    list.appendChild(categoryListItem);
 
-    const checkbox = categoryList.querySelector("input[type='checkbox']");
+    // Attach event listener to checkbox so that user can toggle completed state
+    const checkbox = categoryListItem.querySelector("input[type='checkbox']");
+    checkbox.addEventListener('click', (e) => {
+        markTaskAsComplete(e.target);
+    })
 
+    if (category.completed === true){ // if the task's completed status is true, check the box so user doesn't have to retick it
+        checkbox.checked = true;
+    }
+
+    // Allows the X button to delete with the function below
+  const removeTaskButton = categoryListItem.querySelector(".remove-category-btn");
+  removeTaskButton.addEventListener("click", (e) => {
+    deleteTask(e.target);
+  });
 }
+
+
+// <========== Function that allows user to create a new task  ==========> 
+const addCategoryItem = () => {
+    const categoryListItem = document.createElement("li");
+    categoryListItem.classList = "listItem";
+
+    }
