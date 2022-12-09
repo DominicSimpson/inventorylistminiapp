bccdadd0b856575b32e985ff44f13160537ca41d
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-12-09 16:39:22 +0000

    Initial functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const input = document.querySelector('input'); // Grabs input
+
+const list = document.querySelector('list'); // Grabs list of inputted categories
+
+const submitBtn = document.getElementById('submit-btn'); // Grabs submit button
+
+let categoriesList = JSON.parse(localStorage.getItem('userTasks')) || []; // Array to store the 
+                                                                         // user's inputted categories
+
+// <========== Function that retrieves list of categories from local storage to update DOM  ==========> 
+const getCategoriesFromLocalStorage = (categories) => {
+    // Create a new <li> element and give it a class and unique ID
+    const categoryList = document.createElement("li");
+    categoryList.classList = "listItem";
+    taskItem.dataset.
+}
