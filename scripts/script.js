28b2e18ab2d41bd7c9e92a84b7a6d9e26d28191e
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-12-10 11:09:56 +0000

    JS functionality

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const list = document.querySelector('list'); // Grabs list of inputted categorie
 
 const submitBtn = document.getElementById('submit-btn'); // Grabs submit button
 
-let categoriesList = JSON.parse(localStorage.getItem('userTasks')) || []; // Array to store the 
+let categoriesList = JSON.parse(localStorage.getItem('categoriesList')) || []; // Array to store the 
                                                                          // user's inputted categories
 
 // <========== Function that retrieves list of categories from local storage to update DOM  ==========> 
@@ -12,13 +12,13 @@ const getCategoriesFromLocalStorage = (category) => {
     // Create a new <li> element and give it both a class property and unique ID
     const categoryListItem = document.createElement("li");
     categoryListItem.classList = "listItem";
-    categoryListItem.dataset.taskNum = category.id;
+    categoryListItem.dataset.categoryItemNum = category.id;
 
     // Creates the content inside the <li> element
     categoryListItem.innerHTML = `
     <input aria-label="Mark category as complete" type="checkbox"/>
     <span>${category.description}</span>
-    <button type="button" class="remove-task-btn"><i class="fa-solid fa-x"></i></button>
+    <button type="button" class="remove-category-btn"><i class="fa-solid fa-x"></i></button>
     `;
 
    // Adds the <li> element from the storage to the DOM
@@ -27,22 +27,22 @@ const getCategoriesFromLocalStorage = (category) => {
     // Attach event listener to checkbox so that user can toggle completed state
     const checkbox = categoryListItem.querySelector("input[type='checkbox']");
     checkbox.addEventListener('click', (e) => {
-        markTaskAsComplete(e.target);
+        markCategoryAsComplete(e.target);
     })
 
-    if (category.completed === true){ // if the task's completed status is true, check the box so user doesn't have to retick it
+    if (category.completed === true){ // if the category's completed status is true, check the box so user doesn't have to retick it
         checkbox.checked = true;
     }
 
     // Allows the X button to delete with the function below
-  const removeTaskButton = categoryListItem.querySelector(".remove-category-btn");
-  removeTaskButton.addEventListener("click", (e) => {
-    deleteTask(e.target);
+  const removeCategoryButton = categoryListItem.querySelector(".remove-category-btn");
+  removeCategoryButton.addEventListener("click", (e) => {
+    deleteCategory(e.target);
   });
 }
 
 
-// <========== Function that allows user to create a new task  ==========> 
+// <========== Function that allows user to create a new category  ==========> 
 const addCategoryItem = () => {
     const categoryListItem = document.createElement("li"); // creates a new <li> element in the DOM
     categoryListItem.classList = "listItem"; // accesses CSS
@@ -54,9 +54,9 @@ const addCategoryItem = () => {
 
     // Create the content inside the <li> element
     categoryListItem.innerHTML = `
-    <input aria-label="Mark task as complete" type="checkbox"/>
+    <input aria-label="Mark category as complete" type="checkbox"/>
     <span>${input.value}</span>
-    <button type="button" class="remove-task-btn"><i class="fa-solid fa-x"></i></button>
+    <button type="button" class="remove-category-btn"><i class="fa-solid fa-x"></i></button>
     `;
 
     // Add the <li> element from the storage to the DOM
@@ -76,11 +76,15 @@ const addCategoryItem = () => {
     input.value = ""; // Reset the value
 
     // Allows the X button to delete with the function below
-    const removeTaskButton = categoryListItem.querySelector(".remove-task-btn");
+    const removeCategoryButton = categoryListItem.querySelector(".remove-category-btn");
+    removeCategoryButton.addEventListener("click", (e) => {
+      deleteCategory(e.target);
+    });
 
+    // Attach event listener to checkbox so that user can toggle completed state
+    const checkbox = categoryListItem.querySelector("input[type='checkbox']");
+    checkbox.addEventListener("click", (e) => {
+    markCategoryAsComplete(e.target);
 
-
-
-
-
-  } 
+    })
+} 
