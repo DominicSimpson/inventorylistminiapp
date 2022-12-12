9231d84277da3d3e4d40bd525275f765a9b33d14
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-12-12 14:04:34 +0000

    JS functionality

--- scripts/script.js
+++ scripts/script.js
@@ -20,8 +20,7 @@ const getTasksFromLocalStorage = (task) => {
   <button type="button" class="remove-task-btn"><i class="fa-solid fa-trash-can"></i></button>
   `;
 
-  // Add the <li> element from the storage to the DOM
-  list.appendChild(taskItem);
+  list.appendChild(taskItem); // Add the <li> element from the storage to the DOM
 
   // Allows the dustbin icon to delete with the function below
   const removeTaskButton = taskItem.querySelector(".remove-task-btn");
@@ -31,10 +30,10 @@ const getTasksFromLocalStorage = (task) => {
 }
 
 
-// <========== Function allows user to create a new task  ==========> 
+// <========== Function allows user to create a new category item  ==========> 
 const addTaskItem = () => {
-  // Create a new <li> element and give it a class and unique ID
-  const taskItem = document.createElement("li");
+
+  const taskItem = document.createElement("li"); // Creates a new <li> element and assigns a class and unique ID
   taskItem.classList = "listItem";
   const taskNum = new Date().getTime().toString();
   taskItem.dataset.taskNum = taskNum;
@@ -42,7 +41,7 @@ const addTaskItem = () => {
   if (input.value === "") return;
 
   // Create the content inside the <li> element
-  taskItem.innerHTML = `
+  taskItem.innerHTML = ` 
   <span>${input.value}</span>
   <button type="button" class="remove-task-btn"><i class="fa-solid fa-trash-can"></i></button>
   `;
@@ -50,46 +49,43 @@ const addTaskItem = () => {
   // Add the <li> element from the storage to the DOM
   list.appendChild(taskItem);
 
-  // Create an object made up of the: 1) task's description; 2) task's status; 3) task's ID
+  // Create an object made up of the: 1) category's description; 2) category's Boolean status; 3) category's ID
   const newTask = {
     description: input.value,
     completed: false,
     id: taskNum,
   }
 
-  // Store the object inside the tasks array
-  userTasks.push(newTask);
+  userTasks.push(newTask); // Store the object inside the category array
 
-  // Update the tasks array in local storage
+  // Update the category array in local storage
   localStorage.setItem("userTasks", JSON.stringify(userTasks));
   
-  // Reset the input 
-  input.value = "";
+  input.value = ""; // Resets the input 
 
-  // allows the X button to delete with the function below
+  // Allows the dustbin icon to delete with the function below
   const removeTaskButton = taskItem.querySelector(".remove-task-btn");
   removeTaskButton.addEventListener("click", (e) => {
     deleteTask(e.target);
   });
-
 };
+
 
 // <========== Function allows user to delete a new task  ==========> 
 const deleteTask = (e) => {
   // uses .closest to remove 'closest li element'
   const taskToDelete = e.closest("li");
 
-  // Gets the ID of the task to delete
-  const uniqueID = taskToDelete.dataset.taskNum;
+  const uniqueID = taskToDelete.dataset.taskNum; // Gets the ID of the task to delete
 
-  // Use filter to remove (filter out) the task to remove and update the tasks array
+  // Use filter to remove (filter out) a deleted category in order to remove and update the category array
   userTasks = userTasks.filter(item => item.id !== uniqueID);
 
-  // Update the tasks array in local storage
+  // Update the categories array in local storage
   localStorage.setItem("userTasks", JSON.stringify(userTasks));
 
-  // remove the task
-  taskToDelete.remove();
+  taskToDelete.remove(); // Removes the category
+
 };
 
 // <========== Event Listeners ==========>
